97bca88166c548e3808549a406029e532249d816
JLSystem <joaquimmigueldecristo@gmail.com>
2026-06-21 20:19:56 +0100

    first commit5

--- assets/js/view/galeria/home.js
+++ assets/js/view/galeria/home.js
@@ -20,64 +20,60 @@ export default {
                 full: './assets/img/gallery/006.jpeg',
                 title: '<h4>Imagem 2</h4>'
             }, {
-                thumb: './assets/img/gallery/22.jpeg',
-                full: './assets/img/gallery/22.jpeg',
+                thumb: './assets/img/gallery/001.jpeg',
+                full: './assets/img/gallery/001.jpeg',
                 title: '<h4>Imagem 2</h4>'
             }, {
-                thumb: './assets/img/gallery/12.jpeg',
-                full: './assets/img/gallery/12.jpeg',
+                thumb: './assets/img/gallery/002.jpeg',
+                full: './assets/img/gallery/002.jpeg',
                 title: '<h4>Imagem 2</h4>'
             }, {
-                thumb: './assets/img/gallery/13.jpeg',
-                full: './assets/img/gallery/13.jpeg',
+                thumb: './assets/img/gallery/0013.jpeg',
+                full: './assets/img/gallery/0013.jpeg',
                 title: '<h4>Imagem 3</h4>'
             }, {
-                thumb: './assets/img/gallery/14.jpeg',
-                full: './assets/img/gallery/14.jpeg',
+                thumb: './assets/img/gallery/0014.jpeg',
+                full: './assets/img/gallery/0014.jpeg',
                 title: '<h4>Imagem 1</h4><p>Descrição da imagem</p>'
             }, {
-                thumb: './assets/img/gallery/15.jpeg',
-                full: './assets/img/gallery/15.jpeg',
+                thumb: './assets/img/gallery/0015.jpeg',
+                full: './assets/img/gallery/0015.jpeg',
                 title: '<h4>Imagem 2</h4>'
             }, {
-                thumb: './assets/img/gallery/16.jpeg',
-                full: './assets/img/gallery/16.jpeg',
+                thumb: './assets/img/gallery/0016.jpeg',
+                full: './assets/img/gallery/0016.jpeg',
                 title: '<h4>Imagem 3</h4>'
             }, {
-                thumb: './assets/img/gallery/17.jpeg',
-                full: './assets/img/gallery/71.jpeg',
+                thumb: './assets/img/gallery/0017.jpeg',
+                full: './assets/img/gallery/7001.jpeg',
                 title: '<h4>Imagem 1</h4><p>Descrição da imagem</p>'
             }, {
-                thumb: './assets/img/gallery/18.jpeg',
-                full: './assets/img/gallery/18.jpeg',
+                thumb: './assets/img/gallery/008.jpeg',
+                full: './assets/img/gallery/008.jpeg',
                 title: '<h4>Imagem 2</h4>'
             }, {
-                thumb: './assets/img/gallery/19.jpeg',
-                full: './assets/img/gallery/19.jpeg',
+                thumb: './assets/img/gallery/009.jpeg',
+                full: './assets/img/gallery/009.jpeg',
                 title: '<h4>Imagem 3</h4>'
             }, {
-                thumb: './assets/img/gallery/111.jpeg',
-                full: './assets/img/gallery/111.jpeg',
+                thumb: './assets/img/gallery/009.jpeg',
+                full: './assets/img/gallery/009.jpeg',
                 title: '<h4>Imagem 3</h4>'
             }, {
-                thumb: './assets/img/gallery/112.jpeg',
-                full: './assets/img/gallery/112.jpeg',
+                thumb: './assets/img/gallery/0012.jpeg',
+                full: './assets/img/gallery/0012.jpeg',
                 title: '<h4>Imagem 3</h4>'
             }, {
-                thumb: './assets/img/gallery/113.jpeg',
-                full: './assets/img/gallery/113.jpeg',
+                thumb: './assets/img/gallery/0010.jpeg',
+                full: './assets/img/gallery/0010.jpeg',
                 title: '<h4>Imagem 3</h4>'
             }, {
-                thumb: './assets/img/gallery/114.jpeg',
-                full: './assets/img/gallery/114.jpeg',
+                thumb: './assets/img/gallery/0011.jpeg',
+                full: './assets/img/gallery/0011.jpeg',
                 title: '<h4>Imagem 2</h4>'
-            }, {
-                thumb: './assets/img/gallery/23.jpeg',
-                full: './assets/img/gallery/23.jpeg',
-                title: '<h4>Imagem 3</h4>'
-            }, {
-                thumb: './assets/img/gallery/24.jpeg',
-                full: './assets/img/gallery/24.jpeg',
+            },  {
+                thumb: './assets/img/gallery/0018.jpeg',
+                full: './assets/img/gallery/0018.jpeg',
                 title: '<h4>Imagem 4</h4>'
             }]
         };
